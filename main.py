a66be6dc7faa4d6ed0ec8c04a79917e090043b0e
Max Hilgenberg <71235746+mhilgen@users.noreply.github.com>
2021-11-11 10:11:26 -0500

    Solved Issue #1
    Changed the X color from red to green

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 size_of_board = 600
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
 symbol_thickness = 50
-symbol_X_color = '#EE4035'
+symbol_X_color = '#008000'
 symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
 
