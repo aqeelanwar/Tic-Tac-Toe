927793b8745d310a7ce4fc272a543f085c275633
aheffne <78396431+aheffne@users.noreply.github.com>
2021-11-11 10:35:44 -0500

    Finished Issue 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ size_of_board = 600
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
 symbol_thickness = 50
 symbol_X_color = '#008000'
-symbol_O_color = '#0492CF'
+symbol_O_color = '#FFA500'
 Green_color = '#7BC043'
 
 
